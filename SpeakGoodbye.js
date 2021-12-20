1825df28ef41982a0fccc760a4d8b5d6bab208b2
empirica2021 <93788831+empirica2021@users.noreply.github.com>
2021-12-20 14:11:58 +0100

    Add files via upload

---
+++ SpeakGoodbye.js
@@ -0,0 +1,14 @@
+// JavaScript Document
+(function (window) {
+  var byeSpeaker = {};
+  var speakWord = "Good Bye";
+  byeSpeaker.speak = function (name) {
+  console.log(speakWord + " " + name);
+}
+
+  window.byeSpeaker = byeSpeaker;
+
+})(window);
+
+
+
